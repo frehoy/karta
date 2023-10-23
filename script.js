9b671a46bd1e63e88de1bfd4f28a7a206f2ef3e4
Frederick Hoyles <frederick.hoyles@gmail.com>
2023-10-23 17:00:25 +0200

    Make script.js suck a bit less

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
 // initialize Leaflet
-var map = L.map('map').setView({lon: 12.0, lat: 55.0}, 2);
+var map = L.map('map').setView({ lon: 16.0, lat: 62.0 }, 7);
 
-async function load_isochrone(lat_lon) {
-  let hours = 1;
-  let time = 3600*hours;
+async function load_isochrone(lat_lon, hours) {
+  let seconds = 3600 * hours;
   p = `${lat_lon.lat},${lat_lon.lng}`
-  console.log(p);
+
+  let profile = "car"
+
   let url = new URL("http://localhost:8989/isochrone");
-  //url.searchParams.set('point', '59.34749407695611,18.08845175590946');
   url.searchParams.set('point', p);
-  url.searchParams.set('profile', 'pt');
-  url.searchParams.set('time_limit', time);
-  url.searchParams.set("pt.earliest_departure_time", "2022-05-06T12:00:00.000Z");
+  url.searchParams.set('time_limit', seconds);
+
+
+  // profile specific params
+  if (profile == "pt") {
+    url.searchParams.set('profile', 'pt');
+    url.searchParams.set("pt.earliest_departure_time", "2023-10-23T12:00:00.000Z");
+  } else {
+    url.searchParams.set('profile', 'car');
+  }
   const response = await fetch(url)
   const response_json = await response.json();
   const polygons = response_json.polygons;
-  console.log(polygons);
   return polygons;
 }
 
 async function add_iso_to_map(lat_lon) {
-  const json = await load_isochrone(lat_lon);
-  L.geoJson(json).addTo(map);
+  isochrones = [
+    // green
+    { time: 0.25, color: "#00ff00", opacity: 1.0 },
+    //yellow
+    { time: 0.5, color: "#FFFF00", opacity: 0.5 },
+    // orange
+    { time: 0.75, color: "#ff7800", opacity: 0.25 },
+    // red
+    { time: 1.0, color: "#ff0000", opacity: 0.1 },
+  ]
+  for (i in isochrones) {
+    isochrone = isochrones[i]
+    console.log(isochrone)
+    const json = await load_isochrone(lat_lon, isochrone.time);
+    let style = {
+      "color": isochrone.color,
+      "weight": 0,
+      "opacity": 0.1
+    }
+    L.geoJson(json, { style: style }).addTo(map);
+  }
 }
 
 
 // add the OpenStreetMap tiles
 L.tileLayer(
-  'http://127.0.0.1:8080/tile/{z}/{x}/{y}.png',
+  'http://localhost:8080/tile/{z}/{x}/{y}.png',
   {
     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
   }
 ).addTo(map);
 
 // show the scale bar on the lower left corner
-L.control.scale({imperial: false, metric: true}).addTo(map);
+L.control.scale({ imperial: false, metric: true }).addTo(map);
 
 // show a marker on the map
 L.marker(
-  {lon: 17.638936, lat: 59.858550}
+  { lon: 17.638936, lat: 59.858550 }
 ).bindPopup('The center of the world').addTo(map);
-
-//add_iso_to_map();
 
 var popup = L.popup();
 
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-    add_iso_to_map(e.latlng)
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+  add_iso_to_map(e.latlng)
 }
 
 map.on('click', onMapClick);
